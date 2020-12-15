788a37cef6fb4339e491bce159b6cf43e1e32ec5
Fidio <fidio@fHl.local>
2020-12-16 00:47:38 +0900

    change file

--- MyMath.java
+++ MyMath.java
@@ -1,6 +1,9 @@
 public class MyMath {
+    //インスタンス化を不可にする
+    private MyMath() {}
+
     //二点間の距離を求める
-    static double getDistance(double x1, double y1, double x2, double y2) {
+    public static double getDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
