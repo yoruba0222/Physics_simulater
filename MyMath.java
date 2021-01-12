7e100d64f0f86f00b7fff772f3bf5a4d626de70b
Fidio-lp2 <qdatian9@gmail.com>
2021-01-12 21:47:49 +0900

    MyMathクラスにベクトルの正規化関数を追加

--- MyMath.java
+++ MyMath.java
@@ -1,9 +1,16 @@
 public class MyMath {
-    //インスタンス化を不可にする
-    private MyMath() {}
+          //インスタンス化を不可にする
+          private MyMath() {}
 
-    //二点間の距離を求める
-    public static double getDistance(double x1, double y1, double x2, double y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-    }
+          //二点間の距離を求める
+          public static double getDistance(double x1, double y1, double x2, double y2) {
+                    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+          }
+
+          //ベクトルの正規化を行う
+          public static Vector2 normalizationVector(Vector2 vec) {
+                    double vecLength = getDistance(vec.x, vec.y, 0, 0);
+                    vec.x /= vecLength; vec.y /= vecLength;
+                    return vec;
+          }
 }
