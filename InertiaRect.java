d14cd650920dd5349aefbe385d81b4c96d0027d6
Fidio-lp2 <qdatian9@gmail.com>
2021-01-14 21:49:25 +0900

    慣性モーメントを用いた回転体の表現完成

--- InertiaRect.java
+++ InertiaRect.java
@@ -66,13 +66,13 @@ public class InertiaRect extends Application{
                     this.endPos.x = event.getX();
                     this.endPos.y = event.getY();
                     // こいつがちょっと曲者かもしれませんね
-                    Vector2 powerVec = new Vector2(this.endPos.x-this.startPos.x-rectCenter.x, this.endPos.y-this.startPos.y-rectCenter.y);
+                    Vector2 powerVec = new Vector2(this.endPos.x-rectCenter.x, this.endPos.y-rectCenter.y);
                     Vector2 radiusVec = new Vector2(this.startPos.x-rectCenter.x, this.startPos.y-rectCenter.y);
                     double rad = Math.acos((powerVec.x * radiusVec.x + powerVec.y * radiusVec.y) /
                     (MyMath.getDistance(powerVec.x, powerVec.y, 0, 0) * MyMath.getDistance(radiusVec.x, radiusVec.y, 0, 0)));
 
                     // 力積
-                    double impulse = MyMath.getDistance(powerVec.x, powerVec.y, 0, 0)*MyMath.getDistance(radiusVec.x, radiusVec.y, 0, 0)*Math.sin(rad) ;
+                    double impulse = MyMath.getDistance(powerVec.x, powerVec.y, 0, 0)*MyMath.getDistance(radiusVec.x, radiusVec.y, 0, 0)*Math.sin(rad) *(-1);
                     // Δωの導出
                     double deltaangularVelocity = impulse / inertia; //ここかな
 
@@ -81,16 +81,29 @@ public class InertiaRect extends Application{
                     double len0, len1, radC0, radC1, radS0, radS1, rad0, rad1;
                     len0 = MyMath.getDistance(powerVec.x, powerVec.y, 0, 0);
                     len1 = MyMath.getDistance(radiusVec.x, radiusVec.y, 0, 0);
-                    radC0 = Math.acos(powerVec.x / len0);
-                    radC1 = Math.acos(radiusVec.x / len1);
-                    radS0 = Math.asin(powerVec.y / len0);
-                    radS1 = Math.asin(radiusVec.y / len1);
-                    if (radS0 < 0) radS0 += Math.PI*2;
-                    if (radS1 < 0) radS1 += Math.PI*2;
+                    radC0 = Math.acos((powerVec.x) / len0);
+                    radC1 = Math.acos((radiusVec.x) / len1);
+                    radS0 = Math.asin((powerVec.y) / len0);
+                    radS1 = Math.asin((radiusVec.y) / len1);
+                    if (radS0 < Math.toRadians(0)) {
+                              if (radC0 < Math.toRadians(90)) radS0 += Math.PI*2;
+                              else radS0 = Math.abs(radS0) + Math.PI;
+                    }
+                    if (radS1 < Math.toRadians(0)) {
+                              if (radC1 < Math.toRadians(90)) radS1 += Math.PI*2;
+                              else radS1 = Math.abs(radS1) + Math.PI;
+                    }
                     rad0 = Math.max(radC0, radS0);
                     rad1 = Math.max(radC1, radS1);
 
-                    if (rad0 < rad1) deltaangularVelocity *= -1;
+                    
+
+                    if (rad0 > rad1) deltaangularVelocity *= (-1);
+                    if ((rad0 < Math.toRadians(90) && rad1 > Math.toRadians(270)) || (rad0 > Math.toRadians(270) && rad1 <Math.toRadians(90))) deltaangularVelocity *= (-1);
+
+                    System.out.println(Math.toDegrees(radC1)); //何故かドラッグしないと315が出る.
+                    
+                    if (Double.isNaN(deltaangularVelocity)) deltaangularVelocity = 0;
 
                     angularVelocity += deltaangularVelocity;
           }
