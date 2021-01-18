95bf120e574f5457539cc577a3f328d99cd3f366
ああああ <68190173+Fidio-lp2@users.noreply.github.com>
2021-01-18 09:02:54 +0900

    Update MyMath.java

--- MyMath.java
+++ MyMath.java
@@ -18,4 +18,42 @@ public class MyMath {
           public static double getNorm(Vector2 vec) {
                     return getDistance(vec.x, vec.y, 0, 0);
           }
+
+          public static Vector2 getMinPosition(Vector2 startPos, Vector2 endPos, Vector2 point) {
+
+                    Vector2 leastPosition = new Vector2();
+
+                    //startPos.xの方がendPos.yよりも常に小さいようにする.
+                    if (startPos.x > endPos.x) {
+                              double tmp = startPos.x;
+                              startPos.x = endPos.x;
+                              endPos.x = tmp;
+                              tmp = startPos.y;
+                              startPos.y = endPos.y;
+                              endPos.y = tmp;
+                    }
+
+                    //計算フェーズ
+                    double m = (endPos.y - startPos.y) / (endPos.x - startPos.x);
+                    double crossX = (200.0+200.0*m+m*m*startPos.x-m*startPos.y) / (1+m*m);
+                    double crossY = m*crossX-m*startPos.x+startPos.y;
+                    //double leastDis = MyMath.getDistance(crossX, crossY, pointX(), pointY());
+                    double startDis = MyMath.getDistance(startPos.x, startPos.y, point.x, point.y);
+                    double endDis = MyMath.getDistance(endPos.x, endPos.y, point.x, point.y);
+                    
+                    // 最短距離求めて最短座標求めるフェーズ
+                    if  (crossX > startPos.x && crossX < endPos.x) {
+                              leastPosition.init(crossX, crossY);
+                              //System.out.println(originPoint.getCenterX());
+                    }
+                    else if (startDis < endDis) {
+                              leastPosition.init(startPos.x, startPos.y);
+                    }
+                    else if(endDis < startDis) {
+                              leastPosition.init(endPos.x, endPos.y);
+                    }
+
+                    return leastPosition;
+                    
+          }
 }
