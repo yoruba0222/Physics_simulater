31f9454d720d7f6d1a971f20a49b3667c7ff74f9
Fidio-lp2 <qdatian9@gmail.com>
2021-01-19 01:45:15 +0900

    ２点間のベクトル求める関数

--- Vector2.java
+++ Vector2.java
@@ -1,20 +1,38 @@
 public class Vector2 {
-    double x;
-    double y;
+          double x;
+          double y;
 
-    public Vector2() {}
-
-    public Vector2(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public void init(double x, double y) {
+          //コンストラクタその１
+          public Vector2() {}
+          // コンストラクタその２
+          public Vector2(double x, double y) {
               this.x = x;
               this.y = y;
-    }
+          }
 
-    public Vector2 inverse() {
-              return new Vector2((-1)*this.x, (-1)*this.y);
-    }
+          // 値の代入し直し
+          public void init(double x, double y) {
+                    this.x = x;
+                    this.y = y;
+          }
+
+          // 逆ベクトルの取得
+          public Vector2 inverse() {
+                    return new Vector2((-1)*this.x, (-1)*this.y);
+          }
+
+          // ベクトルの外積
+          public static double cross(Vector2 vec0, Vector2 vec1) {
+                double value;
+
+                double theta = (
+                          Math.acos(
+                                    (vec0.x*vec1.x+vec0.y*vec1.y) / (MyMath.getNorm(vec0)*MyMath.getNorm(vec1))
+                          )
+                );
+
+                value = MyMath.getNorm(vec0) * MyMath.getNorm(vec1) * Math.sin(theta);
+
+                return value;
+          }
 }
